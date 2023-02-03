f5a686d5e7fbf82b3131c138e828034f88b4ba50
linszay <lindseyjlancaster@gmail.com>
2023-02-03 16:47:04 -0600

    added missing documentation

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,21 @@ class BaseModel:
     """creating the base class for the project"""
 
     def __init__(self):
+        """initializing method"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """str function"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
+        """save function"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """to_dict function"""
         result = self.__dict__.copy()
         result['__class__'] = type(self).__name__
         result['created_at'] = self.created_at.isoformt()
