d3e7e0590bc1fc153a184c5c79f6949845529c1a
Maitreya-today <lessismoreclint@gmail.com>
2023-02-07 14:34:20 -0600

    fixes to pycodestyle

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""console.py import cmd"""
+"""console.py import basemodel"""
+
 
 import cmd
 import models
@@ -7,50 +8,79 @@ from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb) "
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, args):
+        """Quit the program"""
+        return True
 
     def do_create(self, args):
-        """Create a new instance of BaseModel, save it, and print the id."""
-        if not args:
+        """Create instance of BaseModel, save it (to the JSON file) print id.
+                Ex: $ create BaseModel"""
+        if len(args) == 0:
             print("** class name missing **")
             return
-
         class_name = args.split()[0]
-        if class_name not in self.classes:
+        if class_name in models.classes:
+            new_model = models.classes[class_name]()
+            new_model.save()
+            print(new_model.id)
+        else:
             print("** class doesn't exist **")
-            return
-
-        obj = self.classes[class_name]()
-        obj.save()
-        print(obj.id)
 
     def do_show(self, args):
-        """Print string rep of an instance based on the class name and id."""
-        if not args:
+        """Print string rep of an instance based on the class name and id.
+                Ex: $ show BaseModel 1234-1234-1234."""
+        if len(args) == 0:
             print("** class name missing **")
             return
-
-        class_name, obj_id = self.get_class_and_id(args)
-        if not obj_id:
+        args = args.split()
+        if len(args) == 1:
             print("** instance id missing **")
             return
-
-        obj = models.storage.get(class_name, obj_id)
-        if not obj:
+        class_name, obj_id = args[0], args[1]
+        if class_name in models.classes:
+            objects = models.storage.all()
+            for key, value in objects.items():
+                if class_name in key and obj_id in key:
+                    print(value)
+                    return
             print("** no instance found **")
-            return
-
-        print(obj)
+        else:
+            print("** class doesn't exist **")
 
     def do_destroy(self, args):
-        """Delete an instance based on the class name and id."""
-        if not args:
+        """Deletes instance based on class name and id (save into JSON file).
+                Ex: $ destroy BaseModel 1234-1234-1234."""
+        if len(args) == 0:
             print("** class name missing **")
             return
-
-        class_name, obj_id = self.get_class_and_id(args)
-        if not obj_id:
+        args = args.split()
+        if len(args) == 1:
             print("** instance id missing **")
             return
+        class_name, obj_id = args[0], args[1]
+        if class_name in models.classes:
+            objects = models.storage.all()
+            for key in list(objects.keys()):
+                if class_name in key and obj_id in key:
+                    models.storage.delete(objects[key])
+                    return
+            print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
 
-        obj = models.storage.get(class_name, obj_)
+    def do_all(self, args):
+        """Prints all string rep of all instances.
+                Ex: $ all BaseModel or $ all."""
+        objects = models.storage.all()
+        obj_list = []
+        if len(args) == 0:
+            for value in objects.values():
+                obj_list.append(str(value))
+        else:
+            class_name = args.split
